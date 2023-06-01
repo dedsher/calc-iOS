58ea94072cb5b9513a6d2e83cb987b5c59943c3b
Roman Shamov <deadsher@mail.ru>
2023-06-02 00:29:05 +0500

    First version of ios calculator

---
+++ gulpfile.js
@@ -0,0 +1,196 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import {stacksvg} from 'gulp-stacksvg';
+import {deleteAsync} from 'del';
+import autoprefixer from 'autoprefixer';
+import browser from 'browser-sync';
+
+// Styles
+
+export const styles = () => {
+  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(browser.stream());
+}
+
+
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin( { collapseWhitespace: true }))
+    .pipe(gulp.dest('build'))
+}
+
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'))
+}
+
+
+// Images
+
+// const optimizeImages = () => {
+//   return gulp.src([
+//     'source/img/content/*.{jpg,png}',
+//     'source/img/decor/*.{jpg,png}'
+//   ], {
+//     base: 'source'
+//   })
+//     .pipe(squoosh())
+//     .pipe(gulp.dest('build'))
+// }
+
+// const copyImages = () => {
+//   return gulp.src([
+//     'source/img/content/*.{jpg,png}',
+//     'source/img/decor/*.{jpg,png}'
+//   ], {
+//     base: 'source'
+//   })
+//     .pipe(gulp.dest('build'))
+// }
+
+
+// Webp
+
+// const createWebp = () => {
+//   return gulp.src([
+//     'source/img/content/*.{jpg,png}',
+//     'source/img/decor/*.{jpg,png}'
+//   ], {
+//     base: 'source'
+//   })
+//     .pipe(squoosh({
+//       webp: {}
+//     }))
+//     .pipe(gulp.dest('build'))
+// }
+
+
+// Svg
+
+const createStack = () => {
+  return gulp.src('source/img/stack/*.svg')
+    .pipe(svgo())
+    .pipe(stacksvg({ output: 'sprite' }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// const svgOptimize = () => {
+//   return gulp.src('source/img/svg/*.svg')
+//     .pipe(svgo())
+//     .pipe(gulp.dest('build/img/svg'))
+// }
+
+// const svgCopy = () => {
+//   return gulp.src('source/img/svg/*.svg')
+//     .pipe(gulp.dest('build/img/svg'))
+// }
+
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    'source/fonts/*.otf',
+    // 'source/*.ico',
+    // 'source/img/favicons/*.*',
+    // 'source/manifest.webmanifest'
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+    done();
+}
+
+
+// Clean
+
+const clean = () => {
+  return deleteAsync('build');
+}
+
+
+// Server
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+const reload = (done) => {
+  browser.reload();
+  done();
+}
+
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles, reload));
+  gulp.watch('source/js/*.js', gulp.series(scripts, reload))
+  gulp.watch('source/*.html', gulp.series(html, reload));
+}
+
+
+// Build
+
+export const build = gulp.series(
+  clean,
+  copy,
+  // optimizeImages,
+  // svgOptimize,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    // createWebp,
+    createStack
+  )
+)
+
+
+export default gulp.series(
+  clean,
+  copy,
+  // copyImages,
+  // svgCopy,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    // createWebp,
+    createStack
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
+);
---
+++ source/js/calc.js
@@ -0,0 +1,146 @@
+console.log('hi');
+let x1 = '';
+let x2 = '';
+let operation = '';
+let func = '';
+let digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+let operators = ['+', '–', 'x', '÷'];
+let funcs = ['AC', '+/-', '%'];
+let screen = document.querySelector('.calculator__input-field');
+let dotFlag = true;
+screen.textContent = '0';
+
+let calculator = document.querySelector('.calculator');
+
+calculator.addEventListener('click', function(event) {
+  let potentialButton = event.target;
+  if (!potentialButton.classList.contains('calculator__button')) return;
+  else {
+    console.log('click');
+    varDetermine(potentialButton);
+    console.log(x1, operation, x2);
+  }
+});
+
+function Render(content) {
+  if (content % 1 !== 0) {
+    screen.textContent = Math.round(content * 100) / 100;
+  }
+  else {
+    screen.textContent = content;
+  }
+}
+
+
+function varDetermine(button) {
+  let variable = button.textContent;
+
+  if (variable === '=') {
+    calculations();
+
+    Render(x1);
+  }
+  else if (operators.includes(variable) && x1 && !x2) {
+    operation = variable;
+  }
+  else if (digits.includes(variable) && !operation) {
+    if (variable === '.' && x1) {
+      if (x1.includes('.')) return;
+      else {
+        x1 += variable;
+
+        Render(x1);
+      }
+    }
+    else {
+      x1 += variable;
+
+      Render(x1);
+    }
+  }
+  else if (digits.includes(variable) && operation) {
+    if (variable === '.' && x2) {
+      if (x2.includes('.')) return;
+      else {
+        x2 += variable;
+
+        Render(x2);
+      }
+    }
+    else {
+      x2 += variable;
+
+      Render(x2);
+    }
+  }
+  else if (funcs.includes(variable)) {
+    func = variable;
+
+    functions();
+  }
+  else if (x1 && operation && operators.includes(variable)) {
+    calculations();
+    operation = variable;
+
+    Render(x1);
+  }
+  else if (variable === "=") {
+    Render(x1);
+  }
+}
+
+function calculations() {
+  x1 = +x1;
+  x2 = +x2;
+
+  switch (operation) {
+    case '+':
+      x1 = x1 + x2;
+      break;
+    case '–':
+      x1 = x1 - x2;
+      break;
+    case 'x':
+      x1 = x1 * x2;
+      break;
+    case '÷':
+      x1 = x1 / x2;
+      break;
+    case '=':
+  }
+
+  x2 = '';
+}
+
+function functions() {
+  switch (func) {
+    case 'AC':
+      x1 = '';
+      x2 = '';
+      operation = '';
+      func = '';
+
+      Render('0');
+      break;
+    case '+/-':
+      x1 = +x1 * -1;
+
+      Render(x1);
+      break;
+    case '%':
+      if (x2) {
+        x2 = x1 / 100 * x2;
+
+        Render(x2);
+        break;
+      }
+
+      x1 = x1 / 100;
+      Render(x1);
+      break;
+  }
+}
+
+function dotAvailability(x) {
+  return x.includes('.');
+}
